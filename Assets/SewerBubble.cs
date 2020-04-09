6b1b774e4765aef401f655c212e8b1fd086fa7d6
ginsta6 <gintazzz9@gmail.com>
2020-04-09 13:24:07 +0300

    SQ-35 added basic cave map

---
+++ Assets/SewerBubble.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SewerBubble : MonoBehaviour
+{
+    [SerializeField] Transform TeleportTo;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = TeleportTo.position;
+           
+
+        }
+    }
+
+}
