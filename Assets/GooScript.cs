41e9a61a96ec7ca82ee5dcb8e372cdf3e5b970f9
ginsta6 <gintazzz9@gmail.com>
2020-05-06 21:56:21 +0300

    SQ-6 created absorbing mechanic

---
+++ Assets/GooScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooScript : MonoBehaviour
+{
+    public int TranformIndex;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Shapeshifting.Transformations[TranformIndex] != true)
+            Shapeshifting.Transformations[TranformIndex] = true;
+        }
+    }
+}
